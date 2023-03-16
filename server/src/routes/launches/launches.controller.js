ab8b1bca186f44d9e748371841654da870cdb452
prateekvarma <prtk.varma@gmail.com>
2023-03-16 15:14:41 -0600

    (inc) Adding a new launch on the backend

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,7 +1,12 @@
-const { getAllLaunches } = require('../../models/launches.model');
+const { getAllLaunches, addNewLaunch } = require('../../models/launches.model');
 
 function httpGetAllLaunches(req, res) {
   return res.status(200).json(getAllLaunches());
+}
+
+function httpAddNewLaunch(req, res) {
+  const launch = req.body;
+  addNewLaunch(launch)
 }
 
 module.exports = {

d359b91536451fc36e21efbd352977f53659d119
prateekvarma <prtk.varma@gmail.com>
2023-03-20 12:08:20 -0600

    Added Date check on POST /launches test case

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -11,16 +11,31 @@ describe('Test GET /launches', () => {
 });
 
 describe('Test POST /launches', () => {
+  const completeLaunchData = {
+    mission: 'USS Enterprise',
+    rocket: 'NCC 1701-D',
+    target: 'Kepler-186 f',
+    launchDate: 'January 4, 2028',
+  };
+
+  const launchDataWithoutDate = {
+    mission: 'USS Enterprise',
+    rocket: 'NCC 1701-D',
+    target: 'Kepler-186 f',
+  };
+
   test('It should respond with 201 created', async () => {
     const response = await request(app)
       .post('/launches')
-      .send({
-        mission: 'USS Enterprise',
-        rocket: 'NCC 1701-D',
-        target: 'Kepler-186 f',
-        launchDate: 'January 4, 2028',
-      })
+      .send(completeLaunchData)
       .expect('Content-Type', /json/)
       .expect(201);
+
+    const requestDate = new Date(completeLaunchData.launchDate).valueOf();
+    const responseDate = new Date(response.body.launchDate).valueOf();
+
+    expect(responseDate).toBe(requestDate);
+
+    expect(response.body).toMatchObject(launchDataWithoutDate);
   });
 });

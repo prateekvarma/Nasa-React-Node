3549c573dc584ee2ffc4ba8460f8b70c7dc0c51c
prateekvarma <prtk.varma@gmail.com>
2023-03-20 11:53:34 -0600

    Added test to check 201 for POST request on /launches

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -9,3 +9,18 @@ describe('Test GET /launches', () => {
       .expect(200);
   });
 });
+
+describe('Test POST /launches', () => {
+  test('It should respond with 201 created', async () => {
+    const response = await request(app)
+      .post('/launches')
+      .send({
+        mission: 'USS Enterprise',
+        rocket: 'NCC 1701-D',
+        target: 'Kepler-186 f',
+        launchDate: 'January 4, 2028',
+      })
+      .expect('Content-Type', /json/)
+      .expect(201);
+  });
+});

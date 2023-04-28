8ccb0af655f9de897e74b4495fd309f9f689dfda
prateekvarma <prtk.varma@gmail.com>
2023-04-28 16:39:20 -0600

    Checked for error responses

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -45,6 +45,11 @@ async function populateLaunches() {
     },
   });
 
+  if(response.status !== 200) {
+    console.log('Problem downloading launch data');
+    throw new Error('Launch data download failed.')
+  }
+
   const launchDocs = response.data.docs;
   for (const launchDoc of launchDocs) {
     const payloads = launchDoc['payloads'];
